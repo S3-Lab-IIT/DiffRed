ced6a41f2cee8b38f642b71519399a858684e740
prarabdhshukla <prarabdh.10@gmail.com>
2023-10-08 11:02:14 +0530

    Computed PCA embeddings APTOS. Prev commit: Completed stress for APTOS

--- Experiments/high_dimensionality_datasets/slurm_job_scripts/compute_PCA.sh
+++ Experiments/high_dimensionality_datasets/slurm_job_scripts/compute_PCA.sh
@@ -3,4 +3,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=hm
 
-python3 compute_PCA.py -d DIV2k --target_dims 10 20 30 40 
+# $1: Dataset name
+# $2: By default, "10 20 30 40"
+
+python3 compute_PCA.py -d $1 --target_dims $2
