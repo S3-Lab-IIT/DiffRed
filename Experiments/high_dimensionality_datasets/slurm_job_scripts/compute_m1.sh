cf638d609acbbe3f8914b225423bf024878a2fa1
prarabdhshukla <prarabdh.10@gmail.com>
2023-10-08 16:26:24 +0530

    Computed RMap Embeddings for APTOS

--- Experiments/high_dimensionality_datasets/slurm_job_scripts/compute_m1.sh
+++ Experiments/high_dimensionality_datasets/slurm_job_scripts/compute_m1.sh
@@ -2,5 +2,12 @@
 #SBATCH --job-name=embeddings
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=hm
+# PASS THE DATASET NAME AS COMMAND LINE ARGUMENT
+# PASS K1 as CLI ARGUMENT $2
+# PASS K2 as CLI ARGUMENT $3
 
-python3 compute_m1.py -d DIV2k --k1 0 1 2 3 4 5 6 7 0 2 3 4 5 8 10 12 15 18 0 2 3 5 8 10 12 15 18 20 25 27 0 2 4 5 8 10 15 16 20 25 30 35 --k2 10 9 8 7 6 5 4 3 20 18 17 16 15 12 10 8 5 2 30 28 27 25 22 20 18 15 12 10 5 3 40 38 36 35 32 30 25 24 20 15 10 5 --max_iter_list False --max_iter 100
+python3 compute_m1.py -d $1 --k1 $2 --k2 $3 --max_iter_list False --max_iter 100 -f "${1}_m1_results" --dr_tech DiffRed
+
+
+# FOR APTOS:
+#--k1 0 1 2 3 4 5 6 7 0 2 3 4 5 8 10 12 15 18 0 2 3 5 8 10 12 15 18 20 25 27 0 2 4 5 8 10 15 16 20 25 30 35 11 --k2 10 9 8 7 6 5 4 3 20 18 17 16 15 12 10 8 5 2 30 28 27 25 22 20 18 15 12 10 5 3 40 38 36 35 32 30 25 24 20 15 10 5 19 --max_iter_list False --max_iter 100
--- Experiments/high_dimensionality_datasets/slurm_job_scripts/compute_rmap_embeddings.sh
+++ Experiments/high_dimensionality_datasets/slurm_job_scripts/compute_rmap_embeddings.sh
@@ -2,5 +2,9 @@
 #SBATCH --job-name=embeddings
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=hm
+# $1: Dataset name
+# $2: Target dimensions: Typically "10 20 30 40"
+# $3: Number of RMaps to compute: typically 20
 
-python3 compute_RMap.py -d DIV2k --target_dims 10 20 30 40 --eta 20
+
+python3 compute_RMap.py -d $1 --target_dims $2 --eta $3
