8f285258a6471d7938273974740bf7daccf361e1
prarabdhshukla <prarabdh.10@gmail.com>
2023-10-08 23:29:07 +0530

    Recomputed Bank M1 for other DR techniques and compiled it

--- Experiments/dimensionality_reduction_metrics/other_dr_techniques/compute_all_m1.sh
+++ Experiments/dimensionality_reduction_metrics/other_dr_techniques/compute_all_m1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # datasets="Bank Cifar10 FMnist Reuters30k geneRNASeq"
-datasets="hatespeech"
-
-algorithms="PCA UMap T-SNE S-PCA K-PCA"
+datasets=$1
+algorithms=$2
+# algorithms="PCA UMap T-SNE S-PCA K-PCA"
 
 common_t_value="10 20 30 40"
 bank_t_value="1 2 3 5 6 7 8 10"
@@ -33,19 +33,19 @@ for dataset in $datasets; do
             else
                 python3 compute_m1.py --save_dir "${save_dir}${algorithm}" --embed_dir $pca_embed_dir --file_name "m1_results_${algorithm}" --dataset $dataset --dr_tech $algorithm --setting def --target_dims 10 20 30 40
             fi
-        elif [ ! $algorithm == "T-SNE" ]; then
-            if [ $dataset == "Bank " ]; then
-                python3 compute_m1.py --save_dir "${save_dir}${algorithm}" --embed_dir $embed_dir --file_name "m1_results_${algorithm}" --dataset $dataset --dr_tech $algorithm --setting all --target_dims $bank_t_value
-            else
-                python3 compute_m1.py --save_dir "${save_dir}${algorithm}" --embed_dir $embed_dir --file_name "m1_results_${algorithm}" --dataset $dataset --dr_tech $algorithm --setting all --target_dims $common_t_value
-            fi
-        else 
-
-            if [ $dataset == "Bank " ]; then
+        elif [ $algorithm == "T-SNE" ]; then
+            if [ $dataset == "Bank" ]; then
                 python3 compute_m1.py --save_dir "${save_dir}${algorithm}" --embed_dir $embed_dir --file_name "m1_results_${algorithm}" --dataset $dataset --dr_tech $algorithm  --setting all --target_dims 2
             
             else
                 python3 compute_m1.py --save_dir "${save_dir}${algorithm}" --embed_dir $embed_dir --file_name "m1_results_${algorithm}" --dataset $dataset --dr_tech $algorithm --setting all --target_dims 2
+            fi
+
+        else 
+            if [ $dataset == "Bank" ]; then
+                python3 compute_m1.py --save_dir "${save_dir}${algorithm}" --embed_dir $embed_dir --file_name "m1_results_${algorithm}" --dataset $dataset --dr_tech $algorithm --setting all --target_dims $bank_t_value
+            else
+                python3 compute_m1.py --save_dir "${save_dir}${algorithm}" --embed_dir $embed_dir --file_name "m1_results_${algorithm}" --dataset $dataset --dr_tech $algorithm --setting all --target_dims $common_t_value
             fi
         fi
 
