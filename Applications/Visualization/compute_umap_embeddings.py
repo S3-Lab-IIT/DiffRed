26f4fb223539ef50ef6900929ea89c8d12d6b117
prarabdhshukla <prarabdh.10@gmail.com>
2023-11-29 16:40:53 +0530

    Completed T-SNE2 for visualization

--- Applications/Visualization/compute_umap_embeddings.py
+++ Applications/Visualization/compute_umap_embeddings.py
@@ -1,3 +1,6 @@
+#BANK---> Setting8
+#
+
 import numpy as np
 import numpy.linalg as LA
 import math as m
@@ -21,12 +24,12 @@ def parse_arguments():
 
     parser=argparse.ArgumentParser(description='Compute and save embedding matrices')
 
-    parser.add_argument('--save_dir', '-s', help='Path to the directory where embeddings are to be saved', default='./tsne_embeddings/')
+    parser.add_argument('--save_dir', '-s', help='Path to the directory where embeddings are to be saved', default='./umap_embeddings/')
     parser.add_argument('--dataset', '-d', help='Dataset')
     parser.add_argument('--data_dir', help='Directory where embeddings of dr_tech are stored')
     parser.add_argument('--target_dims', '-t', help='List of target dimensions seperated by space', nargs='+')
     parser.add_argument('--dr_tech', help='Preprocessing dimensionality reduction technique', choices=['DiffRed', 'PCA'])
-    parser.add_argument('--tsne', help=' Implementation of tsne to use', choices=['multicore', 'cuda'], default='cuda')
+    # parser.add_argument('--tsne', help=' Implementation of tsne to use', choices=['multicore', 'cuda'], default='cuda')
     parser.add_argument('--k1', help='List of k1 values seperated by space', nargs='+')
     # parser.add_argument('--k2', help='List of corresponding k2 values seperated by space', nargs='+')
     parser.add_argument('--max_iter', help='List of max iter values corresponding to k1 and k2', nargs='+')
@@ -39,7 +42,7 @@ def parse_arguments():
     return args
 
 
-def compute_embeddings(dataset:str, DATA_DIR:str,SAVE_DIR:str, dr_tech:str, target_dim:int, k1:int, k2:int, max_iter:int, setting:dict, tsne:str):
+def compute_embeddings(dataset:str, DATA_DIR:str,SAVE_DIR:str, dr_tech:str, target_dim:int, k1:int, k2:int, max_iter:int, setting:dict):
 
     if dr_tech=='DiffRed':
         embed_path=os.path.join(DATA_DIR, dataset, f'{k1}_{k2}_{max_iter}.npy')
@@ -59,13 +62,7 @@ def compute_embeddings(dataset:str, DATA_DIR:str,SAVE_DIR:str, dr_tech:str, targ
             print(f'Embeddings already saved for dataset: {dataset}, dr_tech: {dr_tech}, target dimension: {target_dim}, setting: {setting}')
             return
         
-        if tsne=='cuda':
-            Z=DR_MAPS['T-SNE'](X,SETTINGS['T-SNE'][setting])
-        elif tsne=='multicore':
-            Z=DR_MAPS['M-TSNE'](X,SETTINGS['M-TSNE'][setting])
-        else:
-            print("Invalid tsne implementation")
-        
+        Z=DR_MAPS['UMap'](X,SETTINGS['UMap'][setting])       
         np.save(os.path.join(SAVE_DIR, dr_tech,dataset, f'{dataset}_{target_dim}_{k1}_{k2}.npy'), Z)
     
     elif dr_tech=='PCA':
@@ -89,12 +86,7 @@ def compute_embeddings(dataset:str, DATA_DIR:str,SAVE_DIR:str, dr_tech:str, targ
             return
         
         
-        if tsne=='cuda':
-            Z=DR_MAPS['T-SNE'](X,SETTINGS['T-SNE'][setting])
-        elif tsne=='multicore':
-            Z=DR_MAPS['M-TSNE'](X,SETTINGS['M-TSNE'][setting])
-        else:
-            print("Invalid tsne implementation")
+        Z=DR_MAPS['UMap'](X,SETTINGS['UMap'][setting])   
 
         np.save(os.path.join(SAVE_DIR, dr_tech,dataset, f'{dataset}_{target_dim}.npy'), Z)
     
@@ -117,11 +109,11 @@ def main():
                 max_iter=[int(args.max_iter[0]) for x in range(len(k1))]
             dr_args=list(zip(k1,k2,max_iter))
 
-            results=[pool.apply_async(compute_embeddings, args=(args.dataset, args.data_dir, args.save_dir,args.dr_tech,target_dims[i], k1[i], k2[i], max_iter[i], args.setting, args.tsne )) for i in range(len(k1))]
+            results=[pool.apply_async(compute_embeddings, args=(args.dataset, args.data_dir, args.save_dir,args.dr_tech,target_dims[i], k1[i], k2[i], max_iter[i], args.setting )) for i in range(len(k1))]
 
     else:
 
-        results=[pool.apply_async(compute_embeddings, args=(args.dataset, args.data_dir, args.save_dir, args.dr_tech, target_dims[i], None, None, None, args.setting, args.tsne)) for i in range(len(target_dims)) ]
+        results=[pool.apply_async(compute_embeddings, args=(args.dataset, args.data_dir, args.save_dir, args.dr_tech, target_dims[i], None, None, None, args.setting)) for i in range(len(target_dims)) ]
 
     
 
