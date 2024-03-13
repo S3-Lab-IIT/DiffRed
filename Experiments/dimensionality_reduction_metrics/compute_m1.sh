051916f10f94eaa536ab5dd35c75ce03b3dd690a
prarabdhshukla <prarabdh.10@gmail.com>
2024-03-13 23:07:04 +0530

    Corrected experiment scripts

--- Experiments/dimensionality_reduction_metrics/compute_m1.sh
+++ Experiments/dimensionality_reduction_metrics/compute_m1.sh
@@ -4,4 +4,4 @@
 # PASS K2 values as CLI ARGUMENT $3
 # PASS max_iter values as CLI ARGUMENT $4
 
-python3 compute_m1.py -d $1 --save_dir ./results/M1_results/ -f $1 --k1 $2 --k2 $3 --max_iter_list False --max_iter $4
+python3 compute_m1.py -d $1 --save_dir ./results/M1_results/ -f $2 --k1 $3 --k2 $4 --max_iter_list False --max_iter $5
--- Experiments/dimensionality_reduction_metrics/compute_stress.sh
+++ Experiments/dimensionality_reduction_metrics/compute_stress.sh
@@ -5,4 +5,4 @@
 # PASS K2 values as CLI ARGUMENT $3
 # PASS max_iter values as CLI ARGUMENT $4
 
-python3 compute_stress.py -d $1 --save_dir ./results/Stress_results/ -f $1 --k1 $2 --k2 $3 --max_iter_list False --max_iter $4
+python3 compute_stress.py -d $1 --save_dir ./results/Stress_results/ -f $2 --k1 $3 --k2 $4 --max_iter_list False --max_iter $5
