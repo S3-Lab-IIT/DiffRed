a6211b50f17f11f09f28c0ffb04eaf7622bfc2f0
prarabdhshukla <prarabdh.10@gmail.com>
2024-03-13 23:24:47 +0530

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='diffred',
-    version='0.1.0',
+    version='0.1.1',
     packages=['DiffRed'],
     author='Prarabdh Shukla',
     author_email='prarabdh.10@gmail.com',
